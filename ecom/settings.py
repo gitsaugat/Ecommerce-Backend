f0fc9ebdf7bd73c7612328dc77592d71c1e5558c
gitsaugat <letnotsay@gmail.com>
2021-06-17 22:00:06 +0545

    Update Allowed Hosts

--- ecom/settings.py
+++ ecom/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWRD_HOSTS')
+ALLOWED_HOSTS = ['djreactecommerce.herokuapp.com']
 # Application definition
 CUSTOM_APPS = [
     'store',
